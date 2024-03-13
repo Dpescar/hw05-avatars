3d08ed94fd3e888234e949b620251481c6441a28
Dpescar <pescar_diana@yahoo.com>
2024-03-13 19:46:34 +0200

    main

---
+++ routes/api/contacts.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const ctrl = require("../../controllers/contacts");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
+
+const { schemas } = require("../../models/contact");
+
+router.get("/", authenticate, ctrl.getAll);
+router.get("/:id", authenticate, isValidId, ctrl.getById);
+router.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrl.addContact
+);
+router.put(
+  "/:id",
+  authenticate,
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateContact
+);
+router.patch(
+  "/:id/favorite",
+  authenticate,
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateContactFavorite
+);
+router.delete("/:id", authenticate, isValidId, ctrl.removeContact);
+
+module.exports = router;
